bbcb1224e226547a88e6b5ba69776b1f29332089
harshmishraskugal <harshmishra@skugal.com>
2023-09-29 20:22:24 +0530

    first commit

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -5,7 +5,7 @@ import backgroundShade from "../../assest/backgroudImage.svg";
 const About = () => {
   return (
     <Grid container className="about-section">
-      <Grid item xs={8}>
+      <Grid item xs={12} className="about-heading">
         <p>
           <span className="f-32">All</span>{" "}
           <span className="green-text">About</span>{" "}
@@ -13,7 +13,7 @@ const About = () => {
           <span className="green-text">Experience</span>{" "}
         </p>
       </Grid>
-      <Grid item xs={8}>
+      <Grid item xs={12}>
         <p>
           <button>About me</button>
           <button>Experience</button>
@@ -27,19 +27,19 @@ const About = () => {
         </Grid>
         <Grid xs={12} sm={5}>
           <ul>
-            <li>
+            <li className="f-18">
               Hello! My name is Mohit Sehrawat & I enjoy creating things that
               live on the internet. I'm a passionate Developer, with strong
               administrative & communication skills, good attention to detail &
               the ability to write efficient code.
             </li>
-            <li>
+            <li className="f-18">
               Hello! My name is Mohit Sehrawat & I enjoy creating things that
               live on the internet. I'm a passionate Developer, with strong
               administrative & communication skills, good attention to detail &
               the ability to write efficient code.
             </li>
-            <li>
+            <li className="f-18">
               Hello! My name is Mohit Sehrawat & I enjoy creating things that
               live on the internet. I'm a passionate Developer, with strong
               administrative & communication skills, good attention to detail &
--- src/Container/Header/Header.tsx
+++ src/Container/Header/Header.tsx
@@ -5,20 +5,20 @@ import "./header.css";
 const Header = () => {
   return (
     <Grid container xs={12} className="header">
-      <Grid item xs={3}>
+      <Grid item xs={12} sm={3} md={3}>
         <p className="f-36">
           Harsh <span className="green-text">Mishra</span>
         </p>
       </Grid>
-      <Grid item xs={6} className="navbar">
+      {/* <Grid item xs={6} className="navbar">
         <p className="f-18">HOME</p>
         <p className="f-18">ABOUT</p>
         <p className="f-18">WORK</p>
         <p className="f-18">SKILLS</p>
         <p className="f-18">CONTACT</p>
         <p className="f-18">RESUME</p>
-        {/* <p className="f-18">Logo</p> */}
-      </Grid>
+        <p className="f-18">Logo</p>
+      </Grid> */}
     </Grid>
   );
 };
